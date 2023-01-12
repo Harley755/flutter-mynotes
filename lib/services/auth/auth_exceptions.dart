450844b3918b3082dccf53d97406f1e2a8a62ba9
Brice G <bricegoudalo@gmail.com>
2023-01-12 21:37:54 +0100

    create AuthException file

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+// LOGIN EXCEPTION
+class UserNotFoundAuthExceptions implements Exception {}
+
+class WrongPasswordAuthExceptions implements Exception {}
+
+// REGISTER EXCEPTION
+class WeakPasswordAuthExceptions implements Exception {}
+
+class EmailAlreadyInUseAuthExceptions implements Exception {}
+
+class InvalidEmailAuthExceptions implements Exception {}
+
+// GENERIC EXCEPTIONS
+class GenericAuthExceptions implements Exception {}
+
+class UserNotLoggedInAuthExceptions implements Exception {}
