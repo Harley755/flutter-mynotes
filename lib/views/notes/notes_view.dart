981c02e88a60f15faa2d63539a10dbd016001f4d
Brice G <bricegoudalo@gmail.com>
2023-02-19 15:14:35 +0100

    Add BlocProvider to main.dart

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/contants/routes.dart';
 import 'package:mynotes/enums/menu_action.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storage.dart';
 import 'package:mynotes/views/notes/notes_list_view.dart';
 import 'package:mynotes/views/utilities/dialogs/logout_dialog.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext;
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -42,12 +45,8 @@ class _NotesViewState extends State<NotesView> {
                   case MenuAction.logout:
                     final shouldLogout = await showLogOutDialog(context);
                     if (shouldLogout) {
-                      await AuthService.firebase().logOut();
                       // ignore: use_build_context_synchronously
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                        loginRoute,
-                        (route) => false,
-                      );
+                      context.read<AuthBloc>().add(const AuthEventLogOut());
                     }
                 }
               },
